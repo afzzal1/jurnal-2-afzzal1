25fc3b76ffe8211ac849ab01f1dea8cdee2d9816
Afzzal1 <afzzalfaisal23@gmail.com>
2024-09-28 23:20:40 +0700

    Coding Hello World

--- soal1.java
+++ soal1.java
@@ -4,12 +4,12 @@ class soal1{
     public static void main(String[] args){
         try (Scanner userInput = new Scanner(System.in)){
             System.out.println("Nama:");
-            String Dira = userInput.nextLine(); // Fix nextLine and Scanner variable name
+            String Nama = userInput.nextLine(); // Fix nextLine and Scanner variable name
 
             System.out.println("Usia:");
             String umur = userInput.nextLine(); // Fix Scanner method call
 
-            System.out.println("Halo, " + Dira + "\nTernyata usiamu baru " + 22 + " tahun ya!"); // Fix variable umur
+            System.out.println("Halo, " + Nama + "\nTernyata usiamu baru " + umur + " tahun ya!"); // Fix variable umur
         } catch(Exception e){
             System.out.println(e);
         }
